3bb274e940aec619fc89ec4477f01d590319b42e
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-05-12 13:30:43 +0100

    tailwind paperform component complete

--- src/components/TailwindPaperForm.js
+++ src/components/TailwindPaperForm.js
@@ -4,35 +4,35 @@ import { CheckIcon } from "@heroicons/react/outline";
 const standardFeatures = [
   "24/7 Access",
   "No contracts, freeze your accout when you like",
+  "Access to the free bicycle stations in London & other major cities",
+];
+const premiumFeatures = [
+  "High quality gym classes included",
+  "A 15% discount on monthly gym memberships with our partner gyms",
+  "Free nutrition guide and workout templates",
+];
+const essentialFeatures = [
+  "A 10% discount on monthly gym memberships with our partner gyms",
+  "Access to the top personal trainers in London",
+  "Access to new and modern gym equipment & technology",
   "Access to the top gym & fitness clubs in London",
-];
-const scaleFeatures = [
-  "Pariatur quod similique",
-  "Sapiente libero doloribus",
-  "Vel ipsa esse repudiandae",
-];
-const intermediateFeatures = [
-  "Quia rem est sed impedit magnam",
-  "Dolorem vero ratione voluptates",
-  "Qui sed ab doloribus voluptatem dolore",
-  "Laborum commodi molestiae id et fugiat",
-  "Nam ut ipsa nesciunt culpa modi dolor",
+  "Get news on the latest offers & discounts to email",
 ];
 
 export default function TailwindPaperForm() {
   return (
     <div className="bg-[#212121] border-t-2 border-t-[#e65100]">
-      <div className="pt-12 px-4 sm:px-6 lg:px-8 lg:pt-20">
+      <div className="pt-24 px-4 sm:px-6 lg:px-8 lg:pt-20">
         <div className="text-center">
-          <h2 className="text-lg leading-6 font-semibold text-gray-300 uppercase tracking-wider">
-            Pricing
+          <h2 className="text-lg leading-6 font-semibold text-[#1de9b6] uppercase tracking-wider">
+            Pricing Plans
           </h2>
-          <p className="mt-2 text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">
+          <p className="mt-2 text-3xl font-extrabold text-[#e65100] sm:text-4xl lg:text-5xl">
             The right price for you, whoever you are
           </p>
-          <p className="mt-3 max-w-4xl mx-auto text-xl text-gray-300 sm:mt-5 sm:text-2xl">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
-            numquam eligendi quos odit doloribus molestiae voluptatum.
+          <p className="mt-3 max-w-4xl mx-auto text-xl text-[#1de9b6] sm:mt-5 sm:text-2xl">
+            Join now and get access to the best gyms and fitness clubs in
+            London. With perks and discounts.
           </p>
         </div>
       </div>
@@ -45,37 +45,37 @@ export default function TailwindPaperForm() {
               <div className="mx-auto max-w-md lg:mx-0 lg:max-w-none lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3">
                 <div className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden lg:rounded-none lg:rounded-l-lg">
                   <div className="flex-1 flex flex-col">
-                    <div className="bg-white px-6 py-10">
+                    <div className="bg-[#e65100] px-6 py-10">
                       <div>
                         <h3
-                          className="text-center text-2xl font-medium text-gray-900"
-                          id="tier-hobby">
+                          className="text-center text-2xl font-medium text-[#212121]"
+                          id="tier-standard">
                           Standard
                         </h3>
                         <div className="mt-4 flex items-center justify-center">
-                          <span className="px-3 flex items-start text-6xl tracking-tight text-gray-900">
+                          <span className="px-3 flex items-start text-6xl tracking-tight text-[#212121]">
                             <span className="mt-2 mr-2 text-4xl font-medium">
                               £
                             </span>
-                            <span className="font-extrabold">15</span>
+                            <span className="font-extrabold">15.99</span>
                           </span>
-                          <span className="text-xl font-medium text-gray-500">
+                          <span className="text-xl font-medium text-[#212121]">
                             /month
                           </span>
                         </div>
                       </div>
                     </div>
-                    <div className="flex-1 flex flex-col justify-between border-t-2 border-gray-100 p-6 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
+                    <div className="flex-1 flex flex-col justify-between border-t-2 border-[#212121] p-6 bg-white sm:p-10 lg:p-6 xl:p-10">
                       <ul role="list" className="space-y-4">
                         {standardFeatures.map((feature) => (
                           <li key={feature} className="flex items-start">
                             <div className="flex-shrink-0">
                               <CheckIcon
-                                className="flex-shrink-0 h-6 w-6 text-green-500"
+                                className="flex-shrink-0 h-6 w-6 text-[#e65100]"
                                 aria-hidden="true"
                               />
                             </div>
-                            <p className="ml-3 text-base font-medium text-gray-500">
+                            <p className="ml-3 text-base font-medium text-[#212121]">
                               {feature}
                             </p>
                           </li>
@@ -108,37 +108,37 @@ export default function TailwindPaperForm() {
                       </span>
                     </div>
                   </div>
-                  <div className="bg-white rounded-t-lg px-6 pt-12 pb-10">
+                  <div className="bg-[#1de9b6] rounded-t-lg px-6 pt-12 pb-10">
                     <div>
                       <h3
-                        className="text-center text-3xl font-semibold text-gray-900 sm:-mx-6"
-                        id="tier-growth">
-                        Intermediate
+                        className="text-center text-3xl font-semibold text-[#212121] sm:-mx-6"
+                        id="tier-essential">
+                        Essential
                       </h3>
                       <div className="mt-4 flex items-center justify-center">
-                        <span className="px-3 flex items-start text-6xl tracking-tight text-gray-900 sm:text-6xl">
+                        <span className="px-3 flex items-start text-6xl tracking-tight text-[#212121] sm:text-6xl">
                           <span className="mt-2 mr-2 text-4xl font-medium">
                             £
                           </span>
-                          <span className="font-extrabold">30.99</span>
+                          <span className="font-extrabold">27.99</span>
                         </span>
-                        <span className="text-2xl font-medium text-gray-500">
+                        <span className="text-2xl font-medium text-[#212121]">
                           /month
                         </span>
                       </div>
                     </div>
                   </div>
-                  <div className="border-t-2 border-gray-100 rounded-b-lg pt-10 pb-8 px-6 bg-gray-50 sm:px-10 sm:py-10">
+                  <div className="border-t-2 border-[#e65100] rounded-b-lg pt-10 pb-8 px-6 bg-white sm:px-10 sm:py-10">
                     <ul role="list" className="space-y-4">
-                      {intermediateFeatures.map((feature) => (
+                      {essentialFeatures.map((feature) => (
                         <li key={feature} className="flex items-start">
                           <div className="flex-shrink-0">
                             <CheckIcon
-                              className="flex-shrink-0 h-6 w-6 text-green-500"
+                              className="flex-shrink-0 h-6 w-6 text-[#e65100]"
                               aria-hidden="true"
                             />
                           </div>
-                          <p className="ml-3 text-base font-medium text-gray-500">
+                          <p className="ml-3 text-base font-medium text-[#212121]">
                             {feature}
                           </p>
                         </li>
@@ -148,7 +148,7 @@ export default function TailwindPaperForm() {
                       <div className="rounded-lg shadow-md">
                         <a
                           href="/join"
-                          className="block w-full text-center rounded-lg border border-transparent bg-[#e65100] px-6 py-4 text-xl leading-6 font-medium text-white hover:opacity-75"
+                          className="block w-full text-center rounded-lg border border-transparent bg-[#1de9b6] px-6 py-4 text-xl leading-6 font-medium text-white hover:opacity-75"
                           aria-describedby="tier-growth">
                           JOIN NOW
                         </a>
@@ -160,37 +160,37 @@ export default function TailwindPaperForm() {
               <div className="mt-10 mx-auto max-w-md lg:m-0 lg:max-w-none lg:col-start-6 lg:col-end-8 lg:row-start-2 lg:row-end-3">
                 <div className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden lg:rounded-none lg:rounded-r-lg">
                   <div className="flex-1 flex flex-col">
-                    <div className="bg-white px-6 py-10">
+                    <div className="bg-[#e65100] px-6 py-10">
                       <div>
                         <h3
-                          className="text-center text-2xl font-medium text-gray-900"
-                          id="tier-scale">
-                          Scale
+                          className="text-center text-2xl font-medium text-[#212121]"
+                          id="tier-Premium">
+                          Premium
                         </h3>
                         <div className="mt-4 flex items-center justify-center">
-                          <span className="px-3 flex items-start text-6xl tracking-tight text-gray-900">
+                          <span className="px-3 flex items-start text-6xl tracking-tight text-[#212121]">
                             <span className="mt-2 mr-2 text-4xl font-medium">
-                              $
+                              £
                             </span>
-                            <span className="font-extrabold">349</span>
+                            <span className="font-extrabold">45.99</span>
                           </span>
-                          <span className="text-xl font-medium text-gray-500">
+                          <span className="text-xl font-medium text-[#212121]">
                             /month
                           </span>
                         </div>
                       </div>
                     </div>
-                    <div className="flex-1 flex flex-col justify-between border-t-2 border-gray-100 p-6 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
+                    <div className="flex-1 flex flex-col justify-between border-t-2 border-[#212121] p-6 bg-white sm:p-10 lg:p-6 xl:p-10">
                       <ul role="list" className="space-y-4">
-                        {scaleFeatures.map((feature) => (
+                        {premiumFeatures.map((feature) => (
                           <li key={feature} className="flex items-start">
                             <div className="flex-shrink-0">
                               <CheckIcon
-                                className="flex-shrink-0 h-6 w-6 text-green-500"
+                                className="flex-shrink-0 h-6 w-6 text-[#e65100]"
                                 aria-hidden="true"
                               />
                             </div>
-                            <p className="ml-3 text-base font-medium text-gray-500">
+                            <p className="ml-3 text-base font-medium text-[#212121]">
                               {feature}
                             </p>
                           </li>
